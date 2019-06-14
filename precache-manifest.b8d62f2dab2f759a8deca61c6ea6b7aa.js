139c605632cf25704492301d4f7a8d506a68422e
mhdalmasri <m.almasri@outlook.de>
2019-06-14 14:32:25 +0200

    Updates

---
+++ precache-manifest.b8d62f2dab2f759a8deca61c6ea6b7aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "365045f3af14d1d2750e43ec8ff873a2",
+    "url": "/Numbers-fact/index.html"
+  },
+  {
+    "revision": "b06763711e334fee9614",
+    "url": "/Numbers-fact/static/css/main.ffa3b81a.chunk.css"
+  },
+  {
+    "revision": "6701a2bddd6e717033f9",
+    "url": "/Numbers-fact/static/js/2.174eae5e.chunk.js"
+  },
+  {
+    "revision": "b06763711e334fee9614",
+    "url": "/Numbers-fact/static/js/main.e68ceea1.chunk.js"
+  },
+  {
+    "revision": "39f8261cd62707b41028",
+    "url": "/Numbers-fact/static/js/runtime~main.58de8d44.js"
+  }
+]);
